fbe2be863085ff6f1003735c73515d4941aec175
surya2365 <suryakirangorthi@gmail.com>
2026-05-13 15:46:32 -0500

    Modified feature update

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Surya, learning Git!");
-        System.out.println("This is a new line added to the code.");
-        System.out.println("This is another line added to the code.");
+        System.out.print("This is a new line added to the code.");
+        //System.out.println("This is another line added to the code.");
     }
 }
